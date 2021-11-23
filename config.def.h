a25b46a5b29d3b3eb458f979145a4b8cd19d557e
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 20:23:34 +0530

    color fix

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*		     fg		bg       */
 	[SchemeNorm]  = { "#bbbbbb", "#000000" },
-	[SchemeSel]   = { "#eeeeee", "#464646" },
+	[SchemeSel]   = { "#eeeeee", "#3c3c3c" },
 	[SchemeOut]   = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
